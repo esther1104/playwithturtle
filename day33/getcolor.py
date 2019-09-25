dcb17b81be82d4f9b3b01ec554e43bc9af3ed429
esther Kim <esther4185@gmail.com>
2019-09-25 23:48:17 +0900

    컬러(오류남)

---
+++ day33/getcolor.py
@@ -0,0 +1,35 @@
+import turtle as t
+
+t.setup(500, 500)
+t.shape("turtle")
+
+t.pencolor("black")
+t.pensize(5)
+
+t.fillcolor("red")
+t.begin_fill()
+
+for x in range(2):
+    t.fd(60)
+    t.lt(90)
+t.end_fill()
+
+def print_color(x, y):
+    color = get_pixel_color(x, y)
+    print(color)
+
+def get_pixel_color(x, y):
+    y = -y
+    canvas = t.getcanvas()
+    ids = canvas.find_overlapping(x, y ,x ,y)
+    if ids:
+        index = ids[-1]
+        color = canvas.itemget(index, "fill")
+        if color:
+            return color
+    return "white"
+
+t.onscreenclick(print_color)
+t.done()
+
+print("어디서 오류가 났는지 못 찾겠다")
